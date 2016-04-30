deb8d8c0cb97ea21da1d2482cab7ee8a3cec4281
Martinpr95 <mbassi@transporte.gob.ar>
2016-04-30 00:48:35 -0300

    Con Entity Framework antes del ordenamiento

---
+++ Mvc/DataAccessLayer/SalesERPDAL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Mvc.Models;
+
+namespace Mvc.DataAccessLayer
+{
+    public class SalesERPDAL:DbContext
+    {
+        //DbSet representa la colección de todas las entidades que pueden
+        //ser consultadas desde la base de datos.
+
+        //Cuando usás LINQ contra el DbSet, este se convierte a una consulta
+        //y pega en la base de datos
+        public DbSet<Employee> Empleados { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //IMPORTANTE: El name de la connection String y el nombre de la
+            //clase del DataAcessLayer DEBEN ser los MISMOS.
+            //Para que mapee automáticamente
+            modelBuilder.Entity<Employee>().ToTable("Empleados");
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //Si llamás de otra forma a la connectionString, en el constructor
+        //de tu DAL, ten´s que pasarle a la clase padre, el nombre
+        //de la nueva cnnString
+        public SalesERPDAL()
+            : base("SalesERPDAL")
+        {
+
+        }
+
+
+    }
+}
---
+++ Mvc/Models/EmployeeBusinessLayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mvc.DataAccessLayer;
+
+namespace Mvc.Models
+{
+    public class EmployeeBusinessLayer
+    {
+        public List<Employee> GetEmployees()
+        {
+            //List<Employee> employees = new List<Employee>();
+            //Employee emp = new Employee();
+            //emp.FirstName = "Johnson";
+            //emp.LastName = "fernandez";
+            //emp.Salary = 14000;
+            //employees.Add(emp);
+            //return employees;
+
+            SalesERPDAL salesDal = new SalesERPDAL();
+            return salesDal.Empleados.ToList();            
+        }
+    }
+}
